f1d420a2cad4a27f9b73029ba8e136800d90402d
kuwayamahirotaka <69295590+kuwayamahirotaka@users.noreply.github.com>
2020-09-04 15:35:36 +0900

    usersテーブルの作成
    カラムの保存

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to root_path
+  else
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+  validates :nickname
+  validates :password_confirmation, format: {with: /\A[a-z\d]{6,}+\z/i, message: "is invalid. Input password."}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birth_date
+  end
+
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid. Input email."} ,uniqueness: { case_sensitive: false }
+  validates :password, format: {with: /\A[a-z\d]{8,}+\z/i, message: "is invalid. Input password."}
+
 end
