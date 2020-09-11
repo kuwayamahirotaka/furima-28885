8e644c3fc521fc30d1c7340e0fc0fa60f1fa7d70
kuwayamahirotaka <69295590+kuwayamahirotaka@users.noreply.github.com>
2020-09-11 17:22:22 +0900

    Userモデルテストコード修正#1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,7 @@ describe User do
     context '新規登録がうまくいくとき' do
       it "nicknameとemail、first_name、last_name、first_name_kana、last_name_kana、
       birth_date、passwordとpassword_confirmationが存在すれば登録できる" do
-      end
-      it 'passwordが6文字以上であれば登録できる' do
+      expect(@user).to be_valid
       end
     end
 
@@ -23,6 +22,11 @@ describe User do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'emailに＠が含まれていない' do
+        @user.email.include("@")
+        @user.valid?
+        expect(@user.errors.full_messages).to include("@ can't be included")
       end
       it '重複したemailが存在する場合登録できない' do
         @user.save
